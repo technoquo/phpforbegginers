101f94551811d1d1447597e0a4add34a91325ad0
Leonel <lelobo75@gmail.com>
2022-12-27 14:08:34 +0100

    condicionyboolean

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>   
+
+<head>
     <title>Demo</title>
 </head>
+<style>
+    body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+    }
+</style>
+
 <body>
-    <?php
-     $greeting = "Hello";
+<?php
+      $name = "Dark Matter";
+
+      $read = true;
+
+      if ($read) {
+         $message = "You have read $name";
+      } else {
+        $message = "You have not read $name";
+      }
     ?>
-   <h1><?php echo "$greeting World"; ?></h1>
+   <h1> <?= $message ?></h1>
 </body>
+
 </html>
