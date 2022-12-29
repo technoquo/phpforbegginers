92c2e2ad9955dab395d1728514f82f3e3886daaa
Leonel <lelobo75@gmail.com>
2022-12-29 11:09:54 +0100

    funcion y filtro

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
@@ -7,30 +8,51 @@
 
 <body>
     <?php
-        $books = [
-            [
-                'name' => 'Do Androids Dream of Electric Sheep',
-                'author' => 'Philip K. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'http://example.com'
-            ]
-        ];
+    $books = [
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'releaseYear' => 1968,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary 2 part',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2022,
+            'purchaseUrl' => 'http://example.com'
+        ]
+    ];
+
+    function filterAuthorBy($books, $author)
+    {
+
+        $filtersBooks = [];
+
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filtersBooks[] = $book;
+            }
+        }
+
+        return $filtersBooks;
+    }
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterAuthorBy($books, 'Andy Weir') as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) By (<?= $book['author'] ?>)
                 </a>
             </li>
         <?php endforeach; ?>
     </ul>
 </body>
+
 </html>
