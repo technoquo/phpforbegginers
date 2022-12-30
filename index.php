b4d6d8de95d575d4a5cd549eb1081c68474be2d3
Leonel <lelobo75@gmail.com>
2022-12-30 19:04:37 +0100

    separatelogic

--- index.php
+++ index.php
@@ -1,93 +1,27 @@
-<!doctype html>
-<html lang="en">
+<?php
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary 2 part',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2022,
+        'purchaseUrl' => 'http://example.com'
+    ]
+];
 
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-</head>
+$filtersBooks = array_filter($books, function ($book) {
+    return $book['author'] === 'Andy Weir';
+});
 
-<body>
-    <?php
-    $books = [
-        [
-            'name' => 'Do Androids Dream of Electric Sheep',
-            'author' => 'Philip K. Dick',
-            'releaseYear' => 1968,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'Project Hail Mary',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2021,
-            'purchaseUrl' => 'http://example.com'
-        ],
-        [
-            'name' => 'Project Hail Mary 2 part',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2022,
-            'purchaseUrl' => 'http://example.com'
-        ]
-    ];
-
-    // one way
-    //    $filterAuthorBy = function ($books, $author)
-//     {
-
-//         $filtersBooks = [];
-
-//         foreach ($books as $book) {
-//             if ($book['author'] === $author) {
-//                 $filtersBooks[] = $book;
-//             }
-//         }
-
-//         return $filtersBooks;
-//     };
-
-
-
-
-//     $filtersBooks = $filterAuthorBy($books, 'Andy Weir');
-
-
-// two way
-    // function filter($items, $fn)
-    // {
-
-    //     $filteredItems = [];
-
-    //     foreach ($items as $item) {
-    //         if ($fn($item)) {
-    //             $filteredItems[] = $item;
-    //         }
-    //     }
-
-    //     return $filteredItems;
-    // }
-
-
-
-
-    // $filtersBooks = filter($books, function($book){ 
-    //     return $book['releaseYear'] < 2000;
-
-    // });
-
-       $filtersBooks = array_filter($books, function($book){ 
-         return $book['author'] === 'Andy Weir';
-
-       });
-    ?>
-
-    <ul>
-        <?php foreach ($filtersBooks as $book) : ?>
-            <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) By (<?= $book['author'] ?>)
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-
-</html>
+require 'index.view.php';
